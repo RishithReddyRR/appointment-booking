6561b07b30b68e7953f3951bb36d6aa18d43a68c
Rishith Reddy <rishithreddy432@gmail.com>
2023-09-05 12:29:34 +0530

    book commit

--- src/components/MyBooking.jsx
+++ src/components/MyBooking.jsx
@@ -2,17 +2,22 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import Booking from './Booking'
+import {SyncLoader} from 'react-spinners'
 import '../css/MyBooking.css'
 import '../const.js';
 const MyBooking = () => {
     const {users}=useSelector(state=>state.users)
     let [app,setApp]=useState([12,23])
+    let [dis,setDis]=useState(false)
     const showApp=async()=>{
+        document.querySelector('.app-disp').style.display="flex"
+        setDis(true)
         const {data}=await axios.post(`${global.url}booking/my-bookings`,{
             email:users.isNewuser.email
         })
         setApp(data.booking)
-        document.querySelector('.app-disp').style.display="flex"
+        setDis(false)
+
     }
     const timeSlots = new Map([
         [0, "10-11"],
@@ -33,9 +38,10 @@ const MyBooking = () => {
     </div>
     <div className="app-disp">
         {
-            app.map(booking=>{
+            dis?<SyncLoader cssOverride={{marginLeft:"7vmax"}} color={"red"} style={{marginBottom:"50vh"}} size={"1vmax"}   />:(  app.map(booking=>{
                 return <Booking timeSlots={timeSlots} id={booking[1]} date={booking[0]} email={users.isNewuser.email} setApp={setApp} app={app}/>
-            })
+            }))
+          
         }
     </div>
     </div>
